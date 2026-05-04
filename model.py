8c2c5ee1a643133250a169510d9ff1006f01b661
Sofia Wallgren <sofwall@kth.se>
2026-05-04 13:43:57 +0200

    Gjort saker mer läsliga

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib . pyplot as plt
-from scipy . stats import multivariate_normal
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 
-w0list = np . linspace ( -3.0 , 1.0 , 200)
-w1list = np . linspace ( -2.0 , 2.0 , 200)
-W0arr , W1arr = np . meshgrid ( w0list , w1list )
-pos = np . dstack (( W0arr , W1arr ))
+w0list = np.linspace (-3.0, 1.0, 200)
+w1list = np.linspace (-2.0, 2.0, 200)
+W0arr, W1arr = np.meshgrid (w0list, w1list)
+pos = np.dstack ((W0arr, W1arr))
 
 
 
@@ -15,7 +15,7 @@ pos = np . dstack (( W0arr , W1arr ))
 mu = [0.0, 0.0]
 Cov = [[1.0, 0.5],[0.5, 1.0]]
 
-rv = multivariate_normal ( mu , Cov )
-Wpriorpdf = rv . pdf ( pos )
-plt . contour ( W0arr , W1arr , Wpriorpdf )
-plt . show ()
+rv = multivariate_normal(mu, Cov)
+Wpriorpdf = rv.pdf(pos)
+plt.contour (W0arr, W1arr, Wpriorpdf)
+plt.show()
