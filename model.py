44fe3c7d56a611d536b565e13baf7862c114267c
Sofia Wallgren <sofwall@kth.se>
2026-05-04 13:27:20 +0200

    Gjort en fil

---
+++ model.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib . pyplot as plt
+from scipy . stats import multivariate_normal
+w0list = np . linspace ( -3.0 , 1.0 , 200)
+w1list = np . linspace ( -2.0 , 2.0 , 200)
+W0arr , W1arr = np . meshgrid ( w0list , w1list )
+pos = np . dstack (( W0arr , W1arr ))
+# set your mu vector and Cov array
+rv = multivariate_normal ( mu , Cov )
+Wpriorpdf = rv . pdf ( pos )
+plt . contour ( W0arr , W1arr , Wpriorpdf )
+plt . show ()
