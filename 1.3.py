bef0806a3d95ce055f7201b6f8390153d90b82a2
kahre.simon@gmail.com <kahre.simon@gmail.com>
2026-05-05 11:59:42 +0200

    Think add solution to task 1.3

---
+++ 1.3.py
@@ -0,0 +1,74 @@
+import pylab as pb
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+from scipy.stats import norm
+from math import pi
+from scipy.spatial.distance import cdist
+
+
+# To draw n samples from multivariate Gaussian distribution with mu and Cov :
+# f = np . random . multivariate_normal ( mu , Cov , n )
+
+w0list = np.linspace (-3.0, 3.0, 200)
+w1list = np.linspace (-3.0, 3.0, 200)
+W0arr, W1arr = np.meshgrid (w0list, w1list)
+pos = np.dstack ((W0arr, W1arr))
+
+trainingX = [-1.5, -1.4, -1.3, -1.2, -1.1, 1.1, 1.2, 1.3, 1.4, 1.5]
+trainingT = []
+for x in trainingX:
+    trainingT = trainingT + [-1.2 + 0.9*x + np.random.normal(0,0.2)]
+
+ones = []
+for x in trainingX:
+    ones = ones + [1]
+
+Xext = [ones, trainingX]
+
+wML = np.dot(np.dot(np.linalg.inv(np.transpose(np.dot(Xext,np.transpose(Xext)))), Xext), np.transpose(trainingT))
+
+
+alpha = 2
+beta = 2
+variance = 1 / alpha
+
+
+# set your mu vector and Cov array
+mu = [0, 0]
+Cov = [[variance, 0.0],[0.0, variance]] # Was we do in reality is to multiply the variance by the identity matrix
+
+rv = multivariate_normal(mu, Cov)
+Wpriorpdf = rv.pdf(pos)
+
+likelyhood = np.zeros(W0arr.shape) # Creates all the available points
+for i in range(W0arr.shape[0]):
+    for j in range(W0arr.shape[1]):
+        # Grabs positional values for w0 and w1
+        w0 = W0arr[i,j]
+        w1 = W1arr[i,j]
+
+        temp = 1
+        for k in range(len(trainingX)):
+            temp = temp * norm.pdf(trainingT[k], w0 + w1*trainingX[k], np.sqrt(1/beta)) # Perform the Gaussian product equation as specified in eq 17
+        
+        likelyhood[i,j] = temp # Sets final calculated value in the likelyhood plot
+
+
+plt.figure(1)
+plt.contour (W0arr, W1arr, Wpriorpdf)
+plt.show()
+
+plt.figure(2)
+plt.contour(W0arr, W1arr, likelyhood)
+plt.show()
+
+sInv = np.array([[alpha,0],[0,alpha]]) + beta*np.dot(Xext, np.transpose(Xext))
+mN = beta * np.dot(np.dot(np.linalg.inv(sInv),Xext), np.transpose(trainingT))
+
+rv = multivariate_normal(mN, np.linalg.inv(sInv))
+Wposteriorpdf = rv.pdf(pos)
+
+plt.figure(3)
+plt.contour(W0arr, W1arr, Wposteriorpdf)
+plt.show()
